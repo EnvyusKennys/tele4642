5938d39df04c5aa93a7a28eb55c8ff5b882befdb
EnvyusKennys <45928549+EnvyusKennys@users.noreply.github.com>
2020-07-17 13:52:37 +0700

    Update routing_protocol.py

--- routing_protocol.py
+++ routing_protocol.py
@@ -4,17 +4,20 @@ from ryu.controller import ofp_event
 from ryu.lib.packet import ethernet, arp, ipv4
 from ryu.topology import event
 from ryu.topology.api import get_switch, get_link
+from ryu.ofproto import ofproto_v1_3
 
 
 class ryu(app_manager.Ryuapp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
     def __init__(self):
-        super().__init__()
+        super(ryu, self).__init__(*args, **kwargs)
+
     def add_ip(self):
         pass
-    
+
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        
-
+        k = 4
