14b645a7209c335d049d488526a7b9bbe2bad54c
EnvyusKennys <45928549+EnvyusKennys@users.noreply.github.com>
2020-07-17 13:43:13 +0700

    Update routing_protocol.py

--- routing_protocol.py
+++ routing_protocol.py
@@ -1,1 +1,20 @@
+# tele4642 lab2 fattree routing protocol
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.lib.packet import ethernet, arp, ipv4
+from ryu.topology import event
+from ryu.topology.api import get_switch, get_link
+
+
+class ryu(app_manager.Ryuapp):
+    def __init__(self):
+        super().__init__()
+    def add_ip(self):
+        pass
+    
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        
 
