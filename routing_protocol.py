eac85842d08ae43307dfe570cc0d83a7fc19b70f
EnvyusKennys <45928549+EnvyusKennys@users.noreply.github.com>
2020-07-23 22:40:19 +0700

    ip and port fixed

--- routing_protocol.py
+++ routing_protocol.py
@@ -1,15 +1,13 @@
 # tele4642 lab2 fattree routing protocol
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.lib.packet import ethernet, arp, ipv4
 from ryu.topology import event
 from ryu.ofproto import ofproto_v1_3
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
+from ryu.controller.handler import set_ev_cls
 
 
-class ryu(app_manager.Ryuapp):
+class ryu(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self):
@@ -39,30 +37,35 @@ class ryu(app_manager.Ryuapp):
             datapath.ofproto.OFPP_CONTROLLER, datapath.ofproto.OFPCML_NO_BUFFER)
         self.add_flow(datapath, match, actions, priority=0)
 
-        dpid = datapath.id
+        dpid = str(datapath.id)
 
         if dpid[0:2] == 'cr':    # need to be fixed, for core sw
             for i in range(k):
                 port = 1 + i
-                ip = ('10.0.'+str(i)+'.0', '255.255.255.0')
+                ip = ('10.' + str(i) + '.0.0', '255.255.255.0')
                 self.add_ip(datapath, ip, port, priority=1)
 
         elif dpid[0:2] == 'ag':  # for aggr sw
+            n = int(dpid[4:5])           # pod
+            j = int(dpid[5:6]) + k / 2   # switch
             for i in range(k / 2):
-                port = 1     # unfixed for suffix port
+                # unfixed for suffix port
+                port = 1 + (((i - 2 + j) % (k / 2)) + k / 2)
                 ip = ('0.0.0.' + str(i + 2), '0.0.0.255')
                 self.add_ip(datapath, ip, port, priority=1)
             for i in range(k / 2):
                 port = 1 + i
-                ip = ('10.1.'+str(i)+'.0', '255.255.255.0')
+                ip = ('10.' + str(n) + '.' + str(i) + '.0', '255.255.255.0')
                 self.add_ip(datapath, ip, port, priority=10)
         elif dpid[0:2] == 'ed':
+            n = int(dpid[4:5])
+            j = int(dpid[5:6])
             for i in range(k / 2):
-                port = 1
+                port = 1 + (((i - 2 + j) % (k / 2)) + k / 2)
                 ip = ('0.0.0.' + str(i + 2), '0.0.0.255')
                 self.add_ip(datapath, ip, port, priority=1)
             for i in range(k / 2):
                 port = 1 + i
-                ip = ('10.2.'+str(i)+'.0', '255.255.255.0')
+                ip = ('10.' + str(n) + '.' + str(j) +
+                      '.' + str(i + 2), '255.255.255.0')
                 self.add_ip(datapath, ip, port, priority=10)
-        
