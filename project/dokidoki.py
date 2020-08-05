3a586e6008a7a6476e3b05ff458bd489de96f808
EnvyusKennys <45928549+EnvyusKennys@users.noreply.github.com>
2020-08-05 20:20:29 +0700

    Update dokidoki.py

--- project/dokidoki.py
+++ project/dokidoki.py
@@ -16,37 +16,45 @@ class FatTopo(Topo):
         super(FatTopo, self).__init__(**opts)
         Hs = []
         Ss = []
+        Cs = []
         # generate the core switch and assign the dpid
-        info('*** Add switches\n')
-        switch = self.addSwitch('Switch', dpid='0000000000000001')
-        print('Switch added: dpid = 0000000000000001')
+        # info('*** Add switches\n')
+        # switch = self.addSwitch('Switch', dpid='0000000000000001')
+        # print('Switch added: dpid = 0000000000000001')
 
         # generate the servers
-        info('*** Add servers\n')
+        info('*** Add servers and ctrl switch\n')
         for i in range(1, 4):
             Ss.append(self.addHost('s%s' % i, mac='00:00:00:00:00:0%s' % i))
             print('Server s%s added' % i)
+            Cs.append(self.addSwitch(
+                'Switch', dpid='000000000000000' + str(i)))
+            print('Switch added: dpid = 000000000000000' + str(i))
 
         # generate the hosts
         info('*** Add hosts\n')
         for i in range(1, k + 1):
-            Hs.append(self.addHost('h%s' % i, mac='00:00:00:00:00:0%s' % (i + 3)))
+            Hs.append(self.addHost('h%s' %
+                                   i, mac='00:00:00:00:00:0%s' % (i + 3)))
             print('Host h%s added' % i)
 
         # connect the host with switch
         for i in range(1, k + 1):
-            self.addLink(switch, Hs[i - 1])
+            for j in range(1, 4):
+                self.addLink(Cs[j - 1], Hs[i - 1])
 
         # connect the servers with switch
         for i in range(1, 4):
-            self.addLink(switch, Ss[i - 1])
+            self.addLink(Cs[i - 1], Ss[i - 1])
 
 
 # run the mininet test
 def simpleTest():
     topo = FatTopo(k=num)
-    net = Mininet(topo=topo, link=TCLink, controller=None, autoSetMacs=False, autoStaticArp=True)
-    net.addController('controller', controller=RemoteController, ip="127.0.0.1", port=6633, protocols="OpenFlow13")
+    net = Mininet(topo=topo, link=TCLink, controller=None,
+                  autoSetMacs=False, autoStaticArp=True)
+    net.addController('controller', controller=RemoteController,
+                      ip="127.0.0.1", port=6633, protocols="OpenFlow13")
     net.start()
     CLI(net)
     net.stop()
@@ -55,3 +63,4 @@ def simpleTest():
 if __name__ == '__main__':
     setLogLevel('info')
     simpleTest()
+
