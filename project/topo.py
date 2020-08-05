65095fcd0d2614c0f150280bfe12b0f6827c1cce
EnvyusKennys <45928549+EnvyusKennys@users.noreply.github.com>
2020-08-05 22:38:49 +0700

    Update topo.py

--- project/topo.py
+++ project/topo.py
@@ -5,18 +5,19 @@ from mininet.link import TCLink
 from mininet.node import RemoteController
 from mininet.node import Node
 from mininet.cli import CLI
-
+import os
 import sys
 
 num = int(sys.argv[1])
 
 
 class Topo(Topo):
+    Hs = []
+    Ss = []
+    Cs = []
+
     def __init__(self, k, **opts):
         super(Topo, self).__init__(**opts)
-        Hs = []
-        Ss = []
-        Cs = []
         # generate the core switch and assign the dpid
         # info('*** Add switches\n')
         # switch = self.addSwitch('Switch', dpid='0000000000000001')
@@ -25,28 +26,35 @@ class Topo(Topo):
         # generate the servers
         info('*** Add servers and ctrl switch\n')
         for i in range(1, 4):
-            Ss.append(self.addHost('s%s' %
-                                   (i - 1), mac='00:00:00:00:00:0%s' % i))
+            self.Ss.append(self.addHost('s%s' %
+                                        (i - 1), mac='00:00:00:00:00:0%s' % i))
             print('Server s%s added' % i)
-            Cs.append(self.addSwitch(
-                'ctrlSw' + str(i - 1), dpid='000000000000000' + str(i)))
-            print('Switch added: dpid = 000000000000000' + str(i))
+        self.Cs.append(self.addSwitch(
+            'ctrlSw0', dpid='0000000000000001'))
+        print('Switch added: dpid = 0000000000000001')
 
         # generate the hosts
         info('*** Add hosts\n')
         for i in range(1, k + 1):
-            Hs.append(self.addHost('h%s' %
-                                   i, mac='00:00:00:00:00:0%s' % (i + 3)))
+            self.Hs.append(self.addHost('h%s' %
+                                        i, mac='00:00:00:00:00:0%s' % (i + 3)))
             print('Host h%s added' % i)
 
         # connect the host with switch
         for i in range(1, k + 1):
-            for j in range(1, 4):
-                self.addLink(Cs[j - 1], Hs[i - 1])
+            self.addLink(self.Cs[0], self.Hs[i - 1])
 
         # connect the servers with switch
         for i in range(1, 4):
-            self.addLink(Cs[i - 1], Ss[i - 1])
+            self.addLink(self.Cs[0], self.Ss[i - 1])
+
+    def setOvs(self):
+        self.setproto(self.Cs)
+
+    def setproto(self, list):
+        for sw in list:
+            cmd = "sudo ovs-vsctl set bridge %s protocols=OpenFlow13" % sw
+            os.system(cmd)
 
 
 # run the mininet test
@@ -57,6 +65,7 @@ def simpleTest():
     net.addController('controller', controller=RemoteController,
                       ip="127.0.0.1", port=6633, protocols="OpenFlow13")
     net.start()
+    topo.setOvs()
     CLI(net)
     net.stop()
 
